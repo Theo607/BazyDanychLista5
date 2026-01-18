ee659040eff78a76853fb46adc6f6aa3ab6addfb
Mateusz Smuga <mateusz@smuga.pl>
2026-01-18 18:26:16 +0100

    Clear

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pip install psycopg2-binary bcrypt tabulate
-
-# Uruchomienie SQL w PostgreSQL
-sudo -i -u postgres psql -f db_setup.sql
-
-echo "Baza danych biblioteka została utworzona."
-
